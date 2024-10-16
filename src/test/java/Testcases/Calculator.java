1c14c015fb63f4f3315e71a86d1fd122e4244653
Muyiti Dannier <daniyarmuhid@gmail.com>
2024-10-15 20:00:52 -0400

    New Commit

--- src/test/java/Testcases/Calculator.java
+++ src/test/java/Testcases/Calculator.java
@@ -1,2 +1,25 @@
-package Testcases;public class Calculator {
+package Testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Calculator {
+
+    @Test
+    public void calculatorTest(){
+        System.out.println("This is test1.");
+    }
+
+    @Test
+    public void calculatorTest2(){
+        System.out.println("This is test2.");
+    }
+
+    @Test
+    public void loginWithValidCredentials(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("sauce demo url");
+    }
+
 }
